5faaf4dc2902dc6eb9ed9f506591e4e7dc3b83ac
Florian <61417955+FlorianDebosz@users.noreply.github.com>
2022-05-07 23:02:40 +0200

    Squashed commit of the following:
    commit e95a8387a9c61cede48b9a52663ece1b673a23d9
    Author: Florian <61417955+FlorianDebosz@users.noreply.github.com>
    Date:   Sat May 7 23:00:44 2022 +0200
    
        finalCommit

---
+++ AdventureCoin/Assets/Scripts/WorldAnimation.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class WorldAnimation : MonoBehaviour
+{
+    public Vector3 rotationCoin;
+    void Update()
+    {
+        transform.Rotate(rotationCoin * Time.deltaTime);
+    }
+}
