9857639524438372d02e6a039502bb6502fb562d
Florian <61417955+FlorianDebosz@users.noreply.github.com>
2022-05-13 07:12:15 +0200

    ✨ Coin Collider

---
+++ AdventureCoin/Assets/Scripts/PlayerCollide.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollide : MonoBehaviour
+{
+    public int nbCoin;
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag == "Coin"){
+            Destroy(other.gameObject);
+            nbCoin++;
+        }
+    }
+}
