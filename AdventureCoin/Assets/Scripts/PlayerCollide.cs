b5c65aaf1e6f03efaa86c579aff82f4b82e0e660
Florian <61417955+FlorianDebosz@users.noreply.github.com>
2022-05-13 07:34:23 +0200

    Collider mobs items (#10)
    * ✨ Coin Collider
    
    * ✨ First Snail With All Colliders

---
+++ AdventureCoin/Assets/Scripts/PlayerCollide.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollide : MonoBehaviour
+{
+    public int nbCoin;
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag == "Coin"){
+            Destroy(other.gameObject);
+            nbCoin++;
+        }
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit) {
+        if(hit.gameObject.tag == "SnailDamage"){
+            print("Aie !");
+        }else if(hit.gameObject.tag == "SnailHurted") {
+            print("Coulé !");
+            Destroy(hit.gameObject.transform.parent.gameObject,0.3f);
+        }
+    }
+}
