3f42cc1f45a5af1f17a1b68a567fb9e3d9ba30e7
Florian <61417955+FlorianDebosz@users.noreply.github.com>
2022-06-08 10:58:16 +0200

    🐛 Patch Snails Collider Effects

--- AdventureCoin/Assets/Scripts/PlayerCollide.cs
+++ AdventureCoin/Assets/Scripts/PlayerCollide.cs
@@ -13,6 +13,7 @@ public class PlayerCollide : MonoBehaviour
             Destroy(CoinParticles, 0.5f);
             Destroy(other.gameObject);
             nbCoin++;
+            print(nbCoin);
         }
     }
 
@@ -23,7 +24,7 @@ public class PlayerCollide : MonoBehaviour
             print("Coulé !");
             GameObject SnailsHit = Instantiate(SnailsParticles, hit.transform.position, Quaternion.identity);
             Destroy(SnailsHit, 0.6f);
-            Destroy(hit.gameObject.transform.parent.gameObject,0.3f);
+            Destroy(hit.gameObject.transform.parent.gameObject,0.1f);
         }
     }
 }
