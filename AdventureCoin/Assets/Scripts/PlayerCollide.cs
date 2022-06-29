b71ac29e3e66b8c0bfb6fd2ab12fc0bd67675a28
Florian <61417955+FlorianDebosz@users.noreply.github.com>
2022-06-29 02:29:26 +0200

    Camera management (#17)
    * 🐛 Fixed GingerBread Animation
    
    * 🐛 Fix Camera Position and Blur Effect
    
    * ✨ Camera Follow Player
    
    * 🐛 ✨ Added Camera and Switch Camera
    
    * 🐛 ✨ New Cam Position
    
    * 🐛 Control switch on Camera
    
    * 🐛 Added a delay on the cam switch
    
    * 🐛 Fixed Snail Ground Collide

--- AdventureCoin/Assets/Scripts/PlayerCollide.cs
+++ AdventureCoin/Assets/Scripts/PlayerCollide.cs
@@ -7,33 +7,76 @@ public class PlayerCollide : MonoBehaviour
     public int nbCoin;
     public GameObject pickUpParticles;
     public GameObject snailsParticles;
-
+    public GameObject mainCam,Camera1,Camera2;
     private bool contact = false;
-        private void OnTriggerEnter(Collider other) {
+
+    public PlayController playController;
+    private Collider otherVarEnter, otherVarExit;
+
+    private CharacterController cc;
+
+    private void Start(){
+            playController = GetComponent<PlayController>();
+            cc = GetComponent<CharacterController>();
+    }
+    private void OnTriggerEnter(Collider other) {
+        //Collider with coin
         if(other.gameObject.tag == "Coin"){
             GameObject coinParticles = Instantiate(pickUpParticles, other.transform.position, Quaternion.identity);
             Destroy(coinParticles, 0.5f);
             Destroy(other.gameObject);
             nbCoin++;
-            print(nbCoin);
+        }
+
+        otherVarEnter = other;
+        Invoke("CamOnTriggerEnter",0.2f);
+    }
+
+    private void CamOnTriggerEnter(){   
+        //Camera Manager
+        if(otherVarEnter.gameObject.tag == "Cam1"){
+            Camera1.SetActive(true);
+            playController.camActive = 1;
+            mainCam.SetActive(false);
+        } else if(otherVarEnter.gameObject.tag == "Cam2"){
+            Camera2.SetActive(true);
+            playController.camActive = 2;
+            mainCam.SetActive(false);
+        } 
+    }
+
+    private void OnTriggerExit(Collider other) {
+        otherVarExit = other;
+        Invoke("CamOnTriggerExit",0.2f);
+    }
+
+    private void CamOnTriggerExit(){
+        if(otherVarExit.gameObject.tag == "Cam1"){
+            Camera1.SetActive(false);
+            mainCam.SetActive(true);
+            playController.camActive = 0;
+        }else if(otherVarExit.gameObject.tag == "Cam2"){
+            Camera2.SetActive(false);
+            mainCam.SetActive(true);
+            playController.camActive = 0;
         }
     }
+
 
     private void OnControllerColliderHit(ControllerColliderHit collision) {
         if(collision.gameObject.tag == "SnailDamage"){
             print("Aie !");
-        }else if(collision.gameObject.tag == "SnailHurted" && !contact) {
-            contact = true;
-            GameObject snailsHit = Instantiate(snailsParticles, collision.transform.position, Quaternion.identity);
-            Destroy(snailsHit, 0.6f);
-            Destroy(collision.gameObject.transform.parent.gameObject,0.1f);
-            StartCoroutine("ResetContact");
+        }else if(collision.gameObject.tag == "SnailHurted" && !contact && !cc.isGrounded) {
+                contact = true;
+                GameObject snailsHit = Instantiate(snailsParticles, collision.transform.position, Quaternion.identity);
+                Destroy(snailsHit, 0.6f);
+                Destroy(collision.gameObject.transform.parent.gameObject,0.1f);
+                StartCoroutine("ResetContact");
         }
     }
-
-    //Coroutine permettant d'attendre 0.8 secondes et de réactiver le contact
+        //Coroutine permettant d'attendre 0.8 secondes et de réactiver le contact
     IEnumerator ResetContact() {
-            yield return new WaitForSeconds(0.8f);
-            contact = false;
-    } 
+        yield return new WaitForSeconds(0.8f);
+        contact = false;
+    }
 }
