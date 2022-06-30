07e71bb2d09f0a929a9bc12443db6eeb146e80cb
Florian <61417955+FlorianDebosz@users.noreply.github.com>
2022-06-30 16:37:41 +0200

    🔥 ✨ Added Life Stats and LifeBar in a proper file (#22)

---
+++ AdventureCoin/Assets/Scripts/PlayerInfos.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class PlayerInfos : MonoBehaviour
+{
+    public static PlayerInfos playerInfos;
+    public int playerHealth = 3;
+    public int coinAmount = 0;
+    public Image[] hearts;
+
+    private void Awake() {
+        playerInfos = this;
+    }
+    
+    public void SetHealth(int health){
+        //Change Health Value
+        playerHealth += health;
+        SetHealthBar();
+        if(playerHealth > 3)
+            playerHealth = 3;
+        if(playerHealth < 0)
+            playerHealth = 0;
+    }
+
+    public void GetCoins(){
+        coinAmount ++;
+    }
+
+    public void SetHealthBar(){
+        //Delete Every Hearts
+        foreach(Image img in hearts){
+            img.enabled = false;
+        }
+
+        //Show Hearts
+        for(int i = 0; i<playerHealth; i++){
+            hearts[i].enabled = true;
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
