c37bdbc1d1c3b461fbbbde5f8326d714799503b0
Florian <61417955+FlorianDebosz@users.noreply.github.com>
2022-08-06 00:45:06 +0200

    ✨ 🐛  Added Checkpoint with Animation and a Respawn Script

---
+++ AdventureCoin/Assets/Scripts/CheckpointMgr.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointMgr : MonoBehaviour
+{
+    private Vector3 lastPoint;
+    public static CheckpointMgr checkpointMgr;
+    void Start()
+    {
+        lastPoint = transform.position;
+        checkpointMgr = this;
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag == "checkpoint"){
+             lastPoint = transform.position;
+             other.gameObject.GetComponent<CoinAnim>().enabled = true;
+        }
+    }
+
+    public void Respawn() {
+        transform.position = lastPoint;
+        PlayerInfos.playerInfos.SetHealth(3);
+    }
+}
