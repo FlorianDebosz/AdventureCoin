2d082d742fc5971c769bc04f627f29d4d8db7ec2
Florian <61417955+FlorianDebosz@users.noreply.github.com>
2022-12-07 17:22:09 +0100

    :sparkles: Start to create Navmesh

---
+++ AdventureCoin/Assets/Scripts/AIScripts.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class AIScripts : MonoBehaviour
+{   
+    [Range(0.5f,50f)] //Slider for adapt distanceDectect
+    [SerializeField] private float distanceDectect = 3f;
+
+    //Draw Distance visualisation for coding
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position,distanceDectect);
+    }
+}
