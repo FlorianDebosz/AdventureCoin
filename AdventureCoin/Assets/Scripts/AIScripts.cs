46b1dd1e2e28cd38317ea52ae8c04f964f7cdc9c
Florian <61417955+FlorianDebosz@users.noreply.github.com>
2022-12-08 04:49:56 +0100

    :sparkles: Snails Patrol

--- AdventureCoin/Assets/Scripts/AIScripts.cs
+++ AdventureCoin/Assets/Scripts/AIScripts.cs
@@ -8,6 +8,28 @@ public class AIScripts : MonoBehaviour
 {   
     [Range(0.5f,50f)] //Slider for adapt distanceDectect
     [SerializeField] private float distanceDectect = 3f;
+    [SerializeField] private Transform[] points;
+    private int destinationIndex = 0;
+    NavMeshAgent agent;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        if(agent != null){
+            agent.destination = points[destinationIndex].position;
+        }
+    }
+
+    private void Update() {
+        float dist = agent.remainingDistance;
+        if(dist <= 0.05f){
+            destinationIndex++;
+            if(destinationIndex > points.Length-1){
+                destinationIndex = 0;
+            }
+        }
+        agent.destination = points[destinationIndex].position;
+    }
 
     //Draw Distance visualisation for coding
     private void OnDrawGizmosSelected()
