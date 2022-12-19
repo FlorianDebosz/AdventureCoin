879ea21cb8307b17b85e8172a0ca0876969249b7
Florian <61417955+FlorianDebosz@users.noreply.github.com>
2022-12-19 17:48:00 +0100

    :sparkles: Chick Watch Player

--- AdventureCoin/Assets/Scripts/ChickAI.cs
+++ AdventureCoin/Assets/Scripts/ChickAI.cs
@@ -5,27 +5,41 @@ using UnityEngine;
 public class ChickAI : MonoBehaviour
 {
     private UnityEngine.AI.NavMeshAgent agent;
-    private Transform playerTransform;
+    [SerializeField] private Transform playerTransform;
     [SerializeField] private MeshCollider chickCollider;
 
     [SerializeField] private float moveSpeed;
-
+    [SerializeField] private float actualSpeed;
+    [SerializeField] private Vector3 oldPosition;
+    [SerializeField] private Transform target;
 
     private void Start() {
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
         agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
         agent.speed = moveSpeed;
+        actualSpeed = Vector3.Distance(oldPosition,transform.position) * 100f;
     }
 
-    private void Update() {
+    private void FixedUpdate() {
         if(FriendsScripts.friendsScripts.canMove){
-            Vector3 reachPosition = playerTransform.position - (2*Vector3.right);
-            agent.destination = reachPosition;
-            
-            if(chickCollider){
-                chickCollider.enabled = false; // Still not sure about disabling ChickCollider
+            if(FriendsScripts.friendsScripts.canMove){
+                //Get Actual Speed
+                actualSpeed = Vector3.Distance(oldPosition,transform.position) * 100f;
+                oldPosition = transform.position;
+
+                Vector3 reachPosition = playerTransform.position - (2.5f*Vector3.right);
+                
+                if(Vector3.Distance(transform.position,reachPosition) > 1){
+                    agent.destination = reachPosition;
+                }
+
+                if(chickCollider){
+                    chickCollider.enabled = false; // Still not sure about disabling ChickCollider
+                }
             }
+            
+            //Watch Player
+            transform.rotation = Quaternion.LookRotation(transform.position - target.position);
         }
     }
-
 }
